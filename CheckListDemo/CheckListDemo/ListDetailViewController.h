5535e613527dbee276e98fafaee098b59291f104
ssn <ssnfee@gmail.com>
2014-03-12 11:13:04 +0800

    16,本节收获很丰富 － storyboard id初次使用 － 关于view内嵌nvagationviewcontroller有了新的认识，如果不从nva controller 得到内嵌的vc然后push，直接实例化内嵌vc push会没有导航栏的

---
+++ CheckListDemo/CheckListDemo/ListDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListDetailViewController.h
+//  CheckListDemo
+//
+//  Created by sunydea on 14-3-12.
+//  Copyright (c) 2014年 sunydea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListDetailViewController : UITableViewController
+
+@end
